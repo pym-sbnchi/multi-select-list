4dd6765f3fd174dc8e743fccfdbbf415e02c1805
Payam Saboonchi <140518628+pym-sbnchi@users.noreply.github.com>
2024-04-28 18:04:42 +0330

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import MultiSelectCreatableWithLibrary from "./WithLibrary/MultiSelectCreatableWithLibrary";
 import MultiSelectCreatableWithoutLibrary from "./WithoutLibrary/MultiSelectCreatableWithoutLibrary";
 
